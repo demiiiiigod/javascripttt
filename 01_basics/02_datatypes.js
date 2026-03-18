0304385329bd6ec864633ad9c55faf2f8f95e6bf
demiiiiigod <yadavfree219@gmail.com>
2026-03-18 17:58:01 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// alert(3+3) we are using node js , not browser so it wonnt work
+
+console.log(3+
+    
+    4) // code readability should be high 
+
+let name = "aman"
+let age = 18
+let isuserlogin = true
+
+
+// some data types
+// number -> 2 to power 53
+// string -> ""
+// boolean -> true or false
+// bigint for bigger integer values
+// null -> standalone
+// undefined
+//symbol -> unique
+
+// object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
