ea14bad695920cd3fc4fac78f44c20199b6bf2ab
demiiiiigod <yadavfree219@gmail.com>
2026-03-18 17:32:40 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountid = 123244
+let account_email = "aman@google.com"
+var accountnumber = "123434"
+accountcity= " jaipur"
+ 
+
+// accountid = 1234245  not possible assigning to comnstant variavbles
+
+console.log(accountid)
+
+account_email ="2345365"
+console.table([account_email,accountid,accountnumber,accountcity])
