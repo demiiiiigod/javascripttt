b3ed74c69e0e5e208c60522dc9130bf342d91040
demiiiiigod <yadavfree219@gmail.com>
2026-03-19 17:18:25 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,23 @@
+let score = "45"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valuenumber = Number(score)
+
+console.log(typeof valuenumber);
+console.log( score)
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+let loggedin = 1
+
+let booleanloggedin = Boolean(loggedin)
+console.log(booleanloggedin)
+
+let number = 33
+let stringnumber = String(number)
+console.log(stringnumber)
+console.log(typeof stringnumber)
