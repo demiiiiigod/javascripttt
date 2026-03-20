8ddde99c0c0d641b291c579b7fc5b0d0ea7c0d31
demiiiiigod <yadavfree219@gmail.com>
2026-03-20 10:58:47 +0000

    datatypes_summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,24 @@
+const score = 100
+const scorevalue = 100.3
+const temp = null
+const isloggedin = false
+
+let useremail;
+
+const id = Symbol("123")
+const anotherid = Symbol("123")
+
+
+console.log(id === anotherid)
+
+const bignumber = 135632643223421462147n
+console.log(typeof bignumber)
+
+const heroes = ["shaktiman","lalu yadav", "aman"]
+let obj = {
+    name: aman,
+    age : 18,
+}
+const myfunction = function(){
+    console.log("aman")
+}
