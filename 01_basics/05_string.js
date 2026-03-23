da854aac368070e187a99acbddc20cf040d1f2c5
demiiiiigod <yadavfree219@gmail.com>
2026-03-23 05:28:16 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,7 @@
+const name  = "aman"
+const repocount = 50
+
+console.log(`my name is ${name} and my repo count is ${repocount}`)
+
+const newstring = new String("aman")
+console.log(newstring)
--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -1,24 +1,46 @@
-const score = 100
-const scorevalue = 100.3
-const temp = null
-const isloggedin = false
+// const score = 100
+// const scorevalue = 100.3
+// const temp = null
+// const isloggedin = false
 
-let useremail;
+// let useremail;
 
-const id = Symbol("123")
-const anotherid = Symbol("123")
+// const id = Symbol("123")
+// const anotherid = Symbol("123")
 
 
-console.log(id === anotherid)
+// console.log(id === anotherid)
 
-const bignumber = 135632643223421462147n
-console.log(typeof bignumber)
+// const bignumber = 135632643223421462147n
+// console.log(typeof bignumber)
 
-const heroes = ["shaktiman","lalu yadav", "aman"]
-let obj = {
-    name: aman,
-    age : 18,
+// const heroes = ["shaktiman","lalu yadav", "aman"]
+// let obj = {
+//     name: aman,
+//     age : 18,
+// }
+// const myfunction = function(){
+//     console.log("aman")
+// }
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//  memory type
+// stack (primitive) and heap(non-primitive)
+
+let myyoutube = "amanydv"
+let anothername = myyoutube
+anothername = "raoshabh"
+console.log(myyoutube)
+console.log(anothername)
+
+let userone = {
+    email : "aman@ydv",
+    upi : "user@ybl"
 }
-const myfunction = function(){
-    console.log("aman")
-}
+
+usertwo = userone
+
+usertwo.email = "amanydv"
+
+console.log(userone.email)
+console.log(usertwo.email)
