3ac9905d2813a8a80a8e87c05f11aba4f5b42f59
demiiiiigod <yadavfree219@gmail.com>
2026-03-23 05:28:31 +0000

    number and math

---
+++ 01_basics/06_num_math.js
@@ -0,0 +1,27 @@
+// const score = 400
+// console.log(score)
+// const balance = new Number(400)
+
+// console.log(balance)
+// console.log(balance.toString())
+// console.log(balance.toFixed(1))
+
+// const othernumber = 1123.8966
+// console.log(othernumber.toPrecision(4))
+
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+//++++++++++++++++maths++++++++++++++
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.7))
+// console.log(Math.min(2,3,4,5,6,7))
+// console.log(Math.max(2,3,4,5,6,7))
+const min = 10
+const max = 20
+console.log(Math.random()*(max-min+1))
