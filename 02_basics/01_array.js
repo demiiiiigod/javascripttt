9937e39aa2d97a87e4918ae2de2e969b9cdce898
demiiiiigod <yadavfree219@gmail.com>
2026-03-27 06:22:06 +0000

    array_1

---
+++ 02_basics/01_array.js
@@ -0,0 +1,26 @@
+// // array
+const number = [1,2,3,4,5,6]
+// console.log(number[0])
+
+// const array = new Array(1,23,4,5,56,)
+// console.log(array)
+
+// //array methods
+
+// number.push(6)
+// number.pop(6)
+// number.unshift(4)
+// number.shift()
+
+
+// console.log(number.includes(9))
+// console.log(number.indexOf(9))
+// const newarray = number.join()
+// console.log(newarray)
+
+const myn1 = number.slice(1,4)
+console.log(myn1)
+console.log(number)
+const myn2 = number.splice(1,4)
+console.log(myn2)
+console.log(number)
