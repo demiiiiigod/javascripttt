483727cc443b70477bf8a9238cbfd57216bb1fe1
demiiiiigod <yadavfree219@gmail.com>
2026-03-29 10:46:44 +0000

    objects

--- 02_basics/04_object2.js
+++ 02_basics/04_object2.js
@@ -1,45 +1,54 @@
-const tinderuser = new Object() // singleton
+// const tinderuser = new Object() // singleton
 
-const tinderuser2 = {} // non singleton
-tinderuser2.id = 122432
-tinderuser2.name = "aman"
-console.log(tinderuser)
-console.log(tinderuser2)
+// const tinderuser2 = {} // non singleton
+// tinderuser2.id = 122432
+// tinderuser2.name = "aman"
+// console.log(tinderuser)
+// console.log(tinderuser2)
 
-const employee = {
-    email : "raop99980@gmail.com",
-    fullname : {
-        username : {
-            name : "aman yadav"
-        }
+// const employee = {
+//     email : "raop99980@gmail.com",
+//     fullname : {
+//         username : {
+//             name : "aman yadav"
+//         }
 
-    }
-} 
-console.log(employee.fullname.username.name)
+//     }
+// } 
+// console.log(employee.fullname.username.name)
 
-const obj1 = {1:"a" , 2 : "b"} 
-const obj2 = { 3 : "a" , 4 : "b"} 
-// obj3  = Object.assign( {},obj1,obj2)
-obj3 = {...obj1,...obj2}
-console.log(obj3)
+// const obj1 = {1:"a" , 2 : "b"} 
+// const obj2 = { 3 : "a" , 4 : "b"} 
+// // obj3  = Object.assign( {},obj1,obj2)
+// obj3 = {...obj1,...obj2}
+// console.log(obj3)
 
-const user = [
-    {
-        id : 2,
-        email : "h@gmail.com"
-    }, 
-    {
-        id : 2,
-        email : "h@gmail.com"
-    },
-    {
-        id : 2,
-        email : "h@gmail.com"
-    }
-]
-user[1].email
-console.log(tinderuser2)
-console.log(Object.keys(tinderuser2))
-console.log(Object.values(tinderuser2))
-console.log(Object.entries(tinderuser2))
-console.log(tinderuser2.hasOwnProperty('email'))
+// const user = [
+//     {
+//         id : 2,
+//         email : "h@gmail.com"
+//     }, 
+//     {
+//         id : 2,
+//         email : "h@gmail.com"
+//     },
+//     {
+//         id : 2,
+//         email : "h@gmail.com"
+//     }
+// ]
+// user[1].email
+// console.log(tinderuser2)
+// console.log(Object.keys(tinderuser2))
+// console.log(Object.values(tinderuser2))
+// console.log(Object.entries(tinderuser2))
+// console.log(tinderuser2.hasOwnProperty('email'))
+
+const course = {
+    name :"aman",
+    couseid : 24334,
+}
+console.log(course.name)
+
+const { name} = course
+console.log(name)
