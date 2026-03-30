0d009fbdb22cfec0703a43a06e6089e25d985af2
demiiiiigod <yadavfree219@gmail.com>
2026-03-30 16:16:30 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,15 @@
+// var c = 300
+let a = 300
+
+if(true){
+    let a = 10
+   const b = 30
+//    var  c = 10
+}
+// console.log(a)
+// console.log(b)
+console.log(c);
+
+for(let i = 0; i < Array.length;i++){
+    const element  = array[i];
+}
