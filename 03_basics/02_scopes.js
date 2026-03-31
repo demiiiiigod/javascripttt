bb3bbbe9062e5db01482f29a956d36d57d63b397
demiiiiigod <yadavfree219@gmail.com>
2026-03-31 14:14:18 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,15 +1,49 @@
-// var c = 300
-let a = 300
+// // // var c = 300
+// // let a = 300
 
-if(true){
-    let a = 10
-   const b = 30
-//    var  c = 10
-}
-// console.log(a)
-// console.log(b)
-console.log(c);
+// // if(true){
+// //     let a = 10
+// //    const b = 30
+// // //    var  c = 10
+// // }
+// // // console.log(a)
+// // // console.log(b)
+// // console.log(c);
 
-for(let i = 0; i < Array.length;i++){
-    const element  = array[i];
+// // for(let i = 0; i < Array.length;i++){
+// //     const element  = array[i];
+// // }
+
+// function one (){
+//     const username = "hitesh"
+//     function two(){
+//         const website  = "youtube "
+//         console.log(username)
+//     }
+//     two()
+//     // console.log(website)
+// }
+// one()
+
+// if(true){
+//     const username = "hietsh"
+//     if(username === "hietsh"){
+//         const website = " youtube"
+//         console.log(username + website)
+//     }
+//     // console.log(website)
+// }
+// // console.log(username)
+
+// +++++++++++++interesting+++++++++++++++
+
+addone(5)
+function addone(num){
+    return  num+1
 }
+addone(5)
+// addtwo(5)   we cannot do this
+const addtwo  = function(num){
+    return num +2
+}
+addtwo(5)
